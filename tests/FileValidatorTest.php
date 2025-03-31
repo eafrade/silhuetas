22917e6de7b22e19f5bc9cdd81d4d922a97db1e5
Emerson Frade <4500721@terceiros.mackenzie.br>
2025-03-31 12:55:52 -0300

    fix: adjust test and example filename

--- tests/FileValidatorTest.php
+++ tests/FileValidatorTest.php
@@ -7,33 +7,30 @@ use App\FileValidator;
 
 class FileValidatorTest extends TestCase
 {
-    // Test case where the number of cases is different from the number of items in the second line
     public function testInvalidItemCount(): void
     {
         $filePath = __DIR__ . '/files/item_count.teste';
         $this->assertFalse(FileValidator::validateFileStructure($filePath));
     }
 
-    // Test case where the number of cases exceeds the valid range (1-100)
     public function testCasesOutOfRange0(): void
     {
         $filePath = __DIR__ . '/files/range_0.teste';
         $this->assertFalse(FileValidator::validateFileStructure($filePath));
     }
+
     public function testCasesOutOfRange100(): void
     {
         $filePath = __DIR__ . '/files/range_100.teste';
         $this->assertFalse(FileValidator::validateFileStructure($filePath));
     }
 
-    // Test case where the number of cases is not within the valid range (1-100)
     public function testInvalidCaseCount(): void
     {
         $filePath = __DIR__ . '/files/case_count.teste';
         $this->assertFalse(FileValidator::validateFileStructure($filePath));
     }
 
-    // Test case with valid structure
     public function testValidFile(): void
     {
         $filePath = __DIR__ . '/files/file.teste';
